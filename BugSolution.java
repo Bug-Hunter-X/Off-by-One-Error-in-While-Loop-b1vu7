7dcad1116e16a9be68dd79a7df8c009fce574fd0
BugHunter-X <askzenai@gmail.com>
2024-12-14 09:32:04 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5) {
+            System.out.println(x + 1);
+            x++;
+        }
+    }
+}
